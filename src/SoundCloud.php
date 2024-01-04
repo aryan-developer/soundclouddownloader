96dfff63b79deb5d2e254089a8c35858776730de
Aryan <rynshirvani@gmail.com>
2024-01-04 20:05:11 +0330

    v1.1

--- src/SoundCloud.php
+++ src/SoundCloud.php
@@ -22,25 +22,19 @@ class SoundCloud extends HttpRequest
     {
         try {
             $musics = [];
-            $searchData = json_decode(
-                self::get(
-                    "{$this->soundCloudApiUrl}search/tracks?" . self::arrayToUrlEncode(
-                        [
-                            'q' => $query,
-                            'sc_a_id' => ' 801c8a51-9227-40d9-9c11-ac16f6bca216',
-                            'variant_ids' => '',
-                            'facet' => 'genre',
-                            'user_id' => '367935-565481-285242-514567',
-                            'client_id' => $this->getClientId(),
-                            'limit' => '20',
-                            'offset' => '0',
-                            'linked_partitioning' => '1',
-                            'app_version' => '1667398191',
-                            'app_locale' => 'en'
-                        ]
-                    )
-                )
-            )->collection;
+            $searchData = $this->endpoint("{$this->soundCloudApiUrl}search/tracks", [
+                'q' => $query,
+                'sc_a_id' => ' 801c8a51-9227-40d9-9c11-ac16f6bca216',
+                'variant_ids' => '',
+                'facet' => 'genre',
+                'user_id' => '367935-565481-285242-514567',
+                'client_id' => $this->getClientId(),
+                'limit' => '20',
+                'offset' => '0',
+                'linked_partitioning' => '1',
+                'app_version' => '1667398191',
+                'app_locale' => 'en'
+            ])->collection;
             foreach ($searchData as $music) {
                 if ($music->kind == 'track' and $music->streamable) {
                     $musics[] = [
@@ -50,7 +44,7 @@ class SoundCloud extends HttpRequest
                         'release' => $music->created_at,
                         'genre' => $music->genre,
                         'duration' => $music->duration,
-                        'soundcloudurl' => $music->permalink_url
+                        'soundCloudUrl' => $music->permalink_url
                     ];
                 }
             }
@@ -63,20 +57,13 @@ class SoundCloud extends HttpRequest
     public function getMusic(string $id, bool $exportOriginal = false): bool|stdClass|array
     {
         try {
-            $musicData = json_decode(
-                self::get(
-                    "{$this->soundCloudApiUrl}tracks?" .
-                    self::arrayToUrlEncode(
-                        [
-                            'ids' => $id,
-                            'client_id' => $this->getClientId(),
-                            '%5Bobject%20Object%5D' => '',
-                            'app_version' => '1667398191',
-                            'app_locale' => 'en'
-                        ]
-                    )
-                )
-            )[0];
+            $musicData = $this->endpoint("{$this->soundCloudApiUrl}tracks", [
+                'ids' => $id,
+                'client_id' => $this->getClientId(),
+                '%5Bobject%20Object%5D' => '',
+                'app_version' => '1667398191',
+                'app_locale' => 'en'
+            ])[0] ?? null;
             if (is_null($musicData)) {
                 return false;
             } elseif ($exportOriginal) {
@@ -101,18 +88,23 @@ class SoundCloud extends HttpRequest
     {
         $dataMusic = $this->getMusic($id, true);
         $trackAuthorization = $dataMusic->track_authorization;
-        $url = $dataMusic->media->transcodings[0]->url;
+        $directly = false;
+        if (isset($dataMusic->media->transcodings[1])) {
+            $url = $dataMusic->media->transcodings[1]->url;
+            $directly = true;
+        } else {
+            $url = $dataMusic->media->transcodings[0]->url;
+        }
         $trackName = $dataMusic->title . '.mp3';
-        $data = json_decode(
-            self::get("$url?" . self::arrayToUrlEncode(
-                    [
-                        'client_id' => $this->getClientId(),
-                        'track_authorization' => $trackAuthorization
-                    ]
-                )
-            )
-        );
-        $urls = $this->exportUrl($data->url);
+        $data = $this->endpoint($url, [
+            'client_id' => $this->getClientId(),
+            'track_authorization' => $trackAuthorization
+        ]);
+        if (!$directly) {
+            $urls = $this->exportUrl($data->url);
+        } else {
+            $urls = [$url];
+        }
         $this->downloadAndPlay($urls, $trackName);
     }
 
@@ -125,16 +117,27 @@ class SoundCloud extends HttpRequest
             )
         ) {
             $webPageContent = self::get($url);
-            preg_match_all('/soundcloud:\/\/sounds:(.*?)"/m', $webPageContent, $matches );
-            if (isset($matches[1][0])){
+            preg_match_all('/soundcloud:\/\/sounds:(.*?)"/m', $webPageContent, $matches);
+            if (isset($matches[1][0])) {
                 return $this->getMusic($matches[1][0]);
-            }else{
+            } else {
                 return false;
             }
         } else {
             return false;
         }
         return false;
+    }
+
+    /**
+     * @param string $url
+     * @param array $queryData
+     * @return bool|mixed|string
+     */
+    public function endpoint(string $url = "", array $queryData = []): bool|string|array
+    {
+        $backData = self::get("$url?" . self::arrayToUrlEncode($queryData));
+        return json_decode($backData, true);
     }
 
     private function setClientId(): void
@@ -166,12 +169,10 @@ class SoundCloud extends HttpRequest
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_HEADER, TRUE);
         curl_setopt($ch, CURLOPT_NOBODY, TRUE);
-        $data = curl_exec($ch);
+        curl_exec($ch);
         $fileSize = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-        $httpResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
         return intval($fileSize);
-
     }
 
     private function exportUrl(string $url): array
